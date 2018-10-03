0692cc4966e3e5eee699a5591f493eabed19212a
tulip <tulip31518@outlook.com>
2018-10-03 00:09:26 -0700

    Background

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Boundary boundary;
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().position += movement * speed;
+
+		float boundX = Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax);
+		float boundZ = Mathf.Clamp (GetComponent<Rigidbody> ().position.z, boundary.zMin, boundary.zMax);
+		GetComponent<Rigidbody>().position = new Vector3 (boundX, 0.0f, boundZ);
+	}
+}
