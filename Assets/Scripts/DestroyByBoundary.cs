a7a50cc7960d7b3fe6c530a9254ad55049fc6fb2
tulip <tulip31518@outlook.com>
2018-10-03 11:05:05 -0700

    Bullet

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour {
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.GetComponent<PlayerController>() == null)
+			Destroy (other.gameObject);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,27 @@ public class Boundary{
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
 	public float speed;
+	public float tilt;
 	public Boundary boundary;
+
+	public GameObject shot;
+	private GameObject projectile;
+	public Transform shotSpawn;
+	public float nextFire = 0.5f;
+	public float time = 0f;
+	public float fireDelta = 0.5f;
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time - time > nextFire) {
+			projectile = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+			time = Time.time;
+		}
+	}
+
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -20,5 +38,7 @@ public class PlayerController : MonoBehaviour {
 		float boundX = Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax);
 		float boundZ = Mathf.Clamp (GetComponent<Rigidbody> ().position.z, boundary.zMin, boundary.zMax);
 		GetComponent<Rigidbody>().position = new Vector3 (boundX, 0.0f, boundZ);
+
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0f, 0f, moveHorizontal * -tilt);
 	}
 }
